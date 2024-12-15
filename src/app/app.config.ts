116c28bbc47c2a05dcb8c3b7533e14465db5ecc1
Anatoliy Sysoienko <pistalierro@gmail.com>
2024-12-15 14:15:59 +0200

    Добавил картинки для crud bootstrap

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,16 @@ import {routes} from './app.routes';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {getAuth, provideAuth} from '@angular/fire/auth';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyAXMTFyyjNneDeQQ4SH6WcwcJdlbTs4ArM',
+  authDomain: 'portfolio-7bf66.firebaseapp.com',
+  projectId: 'portfolio-7bf66',
+  storageBucket: 'portfolio-7bf66.firebasestorage.app',
+  messagingSenderId: '121589002779',
+  appId: '1:121589002779:web:e1f7dceff73b2eb38c7ebe'
+};
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
@@ -14,14 +23,8 @@ export const appConfig: ApplicationConfig = {
       anchorScrolling: 'enabled',
     })),
     provideAnimations(),
-    provideFirebaseApp(() => initializeApp({
-      apiKey: 'AIzaSyAXMTFyyjNneDeQQ4SH6WcwcJdlbTs4ArM',
-      authDomain: 'portfolio-7bf66.firebaseapp.com',
-      projectId: 'portfolio-7bf66',
-      storageBucket: 'portfolio-7bf66.firebasestorage.app',
-      messagingSenderId: '121589002779',
-      appId: '1:121589002779:web:e1f7dceff73b2eb38c7ebe'
-    })),
-    provideFirestore(() => getFirestore())
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth())
   ]
 };
